ef2b24b3ed50ac1a373d2fe2cc413432910e0eb5
anetta999 <124580023+anetta999@users.noreply.github.com>
2024-02-25 14:08:04 +0200

    created base modal / login / register / code modals

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { useState } from "react";
+import { RegisterModal } from "../RegisterModal/RegisterModal";
+
 export const Header = () => {
-  return <header className="bg-darkBlue text-lightPink"> Header </header>;
+  const [isModalOpen, setModalOpen] = useState(false);
+  return (
+    <header className="bg-darkBlue text-lightPink">
+      Header
+      <button onClick={() => setModalOpen(true)}>Open</button>
+      {isModalOpen && (
+        <RegisterModal
+          onShow={() => setModalOpen(true)}
+          onClose={() => setModalOpen(false)}
+        />
+      )}
+    </header>
+  );
 };
