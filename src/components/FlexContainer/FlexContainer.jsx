adefa56febc82416e49fa339b50504bf7164db19
TN <tn@macbook-pro-tn.home>
2024-02-27 08:25:29 -0800

    header

--- src/components/FlexContainer/FlexContainer.jsx
+++ src/components/FlexContainer/FlexContainer.jsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 
 export const FlexContainer = ({ className, children }) => {
   return (
-    <div className={cn("flex items-center place-content-between", className)}>
+    <div className={cn("flex items-center justify-between", className)}>
       {children}
     </div>
   );
