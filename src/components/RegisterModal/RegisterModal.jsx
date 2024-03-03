c0ae578df3b99cf9c26d7e02fe327c6dc3cb0120
anetta999 <124580023+anetta999@users.noreply.github.com>
2024-03-03 19:46:50 +0200

    created logic and styles at RegisterModal

--- src/components/RegisterModal/RegisterModal.jsx
+++ src/components/RegisterModal/RegisterModal.jsx
@@ -1,34 +1,80 @@
+"use client";
 import Image from "next/image";
 import LogoIcon from "../../../public/icons/LogoIcon";
 import { BaseModal } from "../BaseModal/BaseModal";
 import rocket from "../../../public/images/rocket-iso-color.png";
 import facebook from "../../../public/images/facebook.png";
 import google from "../../../public/images/google.png";
+import { useForm } from "react-hook-form";
+import CloseIcon from "../../../public/icons/CloseIcon";
 
 export const RegisterModal = ({ onShow }) => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      name: "",
+      phone: "",
+    },
+  });
+  const onSubmit = (data) => console.log(data);
+
   return (
     <>
       <BaseModal onShow={onShow}>
-        <LogoIcon />
-        <Image
-          src={rocket}
-          width={40}
-          height={40}
-          alt="Picture of the rocket"
-        />
-        <p>Зареєструватись</p>
-        <form>
-          <div>
-            <label htmlFor="user-name">Ім’я</label>
-            <input type="text" name="name" id="user-name" />
+        <LogoIcon className="w-[291px] h-[72px] mt-[12px] mb-[40px] mx-auto" />
+        <ul className="flex gap-[17px] justify-center items-center mb-[16px]">
+          <li>
+            <Image
+              src={rocket}
+              width={40}
+              height={40}
+              alt="Picture of the rocket"
+              className="rotate-x-[-56deg]"
+            />
+          </li>
+          <li>
+            <p className="text-[16px] text-[#fff]">Зареєструватись</p>
+          </li>
+        </ul>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="relative flex flex-col gap-[8px] mb-[24px]">
+            <label htmlFor="user-name" className="text-[12px] text-[#fff]">
+              Ім’я
+            </label>
+            <input
+              type="text"
+              id="user-name"
+              {...register("name")}
+              className="auth-input"
+            />
+            <button
+              type="button"
+              className="absolute top-[10px] right-[10px] translate-y-[100%] w-[24px] h-[24px] p-0 bg-[transparent]"
+            >
+              <CloseIcon className="w-[100%] h-[100%] fill-[#fff]" />
+            </button>
           </div>
-          <div>
-            <label htmlFor="user-phone">Телефон</label>
-            <input type="text" name="phone" id="user-phone" />
+          <div className="relative flex flex-col gap-[8px] mb-[31px]">
+            <label htmlFor="user-phone" className="text-[12px] text-[#fff]">
+              Телефон
+            </label>
+            <input
+              type="text"
+              id="user-phone"
+              {...register("phone")}
+              className="auth-input"
+            />
+            <button
+              type="button"
+              className="absolute top-[10px] right-[10px] translate-y-[100%] w-[24px] h-[24px] p-0 bg-[transparent]"
+            >
+              <CloseIcon className="w-[100%] h-[100%] fill-[#fff]" />
+            </button>
           </div>
-          <button type="submit">Зареєструватись</button>
+          <button type="submit" className="submit-btn">
+            Зареєструватись
+          </button>
         </form>
-        <ul>
+        <ul className="flex gap-[48px] justify-center">
           <li>
             <button type="button">
               <Image
