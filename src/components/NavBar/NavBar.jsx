86c0aeabff8700d11c5e2e39fda0c7ce4b144d1b
TN <tn@macbook-pro-tn.home>
2024-03-10 14:21:29 -0700

    navigation for loggedin user

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { IconWrapper } from "../IconWrapper/IconWrapper";
 import CloseIcon from "../../../public/icons/CloseIcon";
 import { FlexContainer } from "../FlexContainer/FlexContainer";
@@ -7,9 +7,13 @@ import Link from "next/link";
 import LogoMobile from "../../../public/icons/LogoMobile";
 import Rocket from "../../../public/icons/Rocket";
 import { Footer } from "../FooterMobile/FooterMobile";
-import { navLinks } from "@/data/navLinks";
+import { Navigation } from "./Navigation";
+import { NavLoggedIn } from "./NavLoggedIn";
+import { UserInfo } from "../UserInfo/UserInfo";
 
 export const NavBar = ({ menuOpen, setMenuOpen }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
     if (menuOpen) {
       document.body.style.overflow = "hidden";
@@ -49,58 +53,63 @@ export const NavBar = ({ menuOpen, setMenuOpen }) => {
           </IconWrapper>
         </FlexContainer>
 
-        <FlexContainer
-          className={"pl-[30px] py-5 mb-4 bg-[#9843BD0D] max-w-full"}
-        >
-          <Rocket />
-
-          <Link
-            href="/login/?modal=true"
-            className="text-[16px] leading-[1.2] ml-5 mr-4"
+        {isLoggedIn ? (
+          <UserInfo />
+        ) : (
+          <FlexContainer
+            className={"pl-[30px] py-5 mb-4 bg-[#9843BD0D] max-w-full"}
           >
-            Увійти
-          </Link>
-          <div className="h-[56px] w-[1.5px] bg-white mr-4 element"></div>
-          <Link
-            href="/register/?modal=true"
-            className="text-[16px] leading-[1.2] "
-          >
-            Зареєструватись
-          </Link>
-        </FlexContainer>
+            <Rocket />
 
-        <ul className="flex flex-col items-center gap-2 w-full">
-          {navLinks.map((link) => {
-            return (
-              <li
-                key={link.name}
-                className="h-[40px] w-full bg-gradient-to-r from-[#161C2A] via-[#990078] via-50% to-[#161C2A] to-100% ..."
-              >
-                <Link
-                  href={link.href}
-                  className="flex items-center gap-6 pl-9 py-2 w-full h-[98%] bg-darkBlue"
-                >
-                  {link.icon}
-                  <p>{link.name}</p>
-                </Link>
-              </li>
-            );
-          })}
-          <li className="h-[40px] w-full bg-gradient-to-r from-[#161C2A] via-[#990078] via-50% to-[#161C2A] to-100% ...">
-            <label className="flex gap-6 pl-[40px]  py-2 w-full h-[98%] bg-darkBlue">
-              <input type="checkbox" />
-              Хлопчик
-            </label>
-          </li>
-          <li className="h-[40px] w-full bg-gradient-to-r from-[#161C2A] via-[#990078] via-50% to-[#161C2A] to-100% ...">
-            <label className="flex gap-6 pl-[40px]  py-2 w-full h-[98%] bg-darkBlue">
-              <input type="checkbox" />
-              Дівчинка
-            </label>
-          </li>
-        </ul>
+            <Link
+              href="/login/?modal=true"
+              className="text-[16px] leading-[1.2] ml-5 mr-4"
+            >
+              Увійти
+            </Link>
+            <div className="h-[56px] w-[1.5px] bg-white mr-4 element"></div>
+            <Link
+              href="/register/?modal=true"
+              className="text-[16px] leading-[1.2] "
+            >
+              Зареєструватись
+            </Link>
+          </FlexContainer>
+        )}
+        {isLoggedIn ? <NavLoggedIn /> : <Navigation />}
         <Footer />
       </div>
     </div>
   );
 };
+
+//<ul className="flex flex-col items-center gap-2 w-full">
+//   {navLinks.map((link) => {
+//     return (
+//       <li
+//         key={link.name}
+//         className="h-[40px] w-full bg-gradient-to-r from-[#161C2A] via-[#990078] via-50% to-[#161C2A] to-100% ..."
+//       >
+//         <Link
+//           href={link.href}
+//           className="flex items-center gap-6 pl-9 py-2 w-full h-[98%] bg-darkBlue"
+//         >
+//           {link.icon}
+//           <p>{link.name}</p>
+//         </Link>
+//       </li>
+//     );
+//   })}
+//   <li className="h-[40px] w-full bg-gradient-to-r from-[#161C2A] via-[#990078] via-50% to-[#161C2A] to-100% ...">
+//     <label className="flex gap-6 pl-[40px]  py-2 w-full h-[98%] bg-darkBlue">
+//       <input type="checkbox" />
+//       Хлопчик
+//     </label>
+//   </li>
+//   <li className="h-[40px] w-full bg-gradient-to-r from-[#161C2A] via-[#990078] via-50% to-[#161C2A] to-100% ...">
+//     <label className="flex gap-6 pl-[40px]  py-2 w-full h-[98%] bg-darkBlue">
+//       <input type="checkbox" />
+//       Дівчинка
+//     </label>
+//   </li>
+// </ul>
---
+++ src/components/UserInfo/UserInfo.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { FlexContainer } from "../FlexContainer/FlexContainer";
+import ProfileIcon from "../../../public/icons/ProfileIcon";
+
+export const UserInfo = () => {
+  return (
+    <FlexContainer className={"pl-[30px] py-5 mb-4 bg-[#9843BD0D] max-w-full items-start gap-4"}>
+      {/* <Image
+        src="/public/images/facebook.png"
+        alt={"user profile image"}
+        width={72}
+        height={72}
+        className="rounded-[61px]"
+      /> */}
+          <ProfileIcon width={ "72"} height={"72"} />
+      <div className="flex flex-col">
+        <p className="text-base">User Name</p>
+        <p className="text-xs">email address</p>
+      </div>
+    </FlexContainer>
+  );
+};
