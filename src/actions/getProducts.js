633140929d5b0a9e624f39c0633a9f36edf59165
TN <tn@macbook-pro-tn.home>
2024-03-21 12:49:00 -0700

    set mobile menu opening for registered user and logout

--- src/actions/getProducts.js
+++ src/actions/getProducts.js
@@ -1,22 +1,25 @@
-'use server'
+'use server';
 
 export const getProducts = () => {
-    return fetch("https://marketplace-5ihn.onrender.com/api/v1/products/s/list", {
-        method: 'GET',
-        headers: {
-            'Content-Type':'application/json'
-        }
-    }).then(response => {
-        if (!response.ok) {
-            throw new Error()
-        }
-        console.log(response)
-        console.log('getProducts')
-        return response.json()
-    }).then(data => {
-        // console.log(data)
-        return data
-    }).catch(error => {
-        console.log(error.message);
+  return fetch('https://marketplace-5ihn.onrender.com/api/v1/products/s/list', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error();
+      }
+      console.log(response);
+      console.log('getProducts');
+      // return response.json()
     })
-}
+    .then(data => {
+      // console.log(data)
+      return data;
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
+};
--- src/components/CodeModal/verificationAction.jsx
+++ src/components/CodeModal/verificationAction.jsx
@@ -1,30 +1,32 @@
-"use server";
-import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
+'use server';
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
+import { setCookie } from 'cookies-next';
 
 export default async function verificationAction(fulfilledData) {
   const res = await fetch(
-    "https://marketplace-5ihn.onrender.com/api/v1/auth/login/code",
+    'https://marketplace-5ihn.onrender.com/api/v1/auth/login/code',
     {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(fulfilledData),
-    }
+    },
   );
 
   const json = await res.json();
   console.log(json);
 
-  cookies().set("Authorization", json.token, {
+  cookies().set('Authorization', json.token, {
     secure: true,
     httpOnly: true,
     expires: Date.now() + 24 * 60 * 60 * 1000 * 3,
-    path: "/",
-    sameSite: "strict",
+    path: '/',
+    sameSite: 'strict',
   });
 
   if (res.ok) {
-    redirect("/");
+    // setCookie('key', 'value', { req, res });
+    redirect('/');
   } else {
     const error = new Error(json.error?.message);
     error.response = json.error;
