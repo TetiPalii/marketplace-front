8f2b55ac1641e2f1d6df8d3e73e0679afccc2998
TN <tn@macbook-pro-tn.home>
2024-03-26 06:47:50 -0700

    fiixed

--- src/components/NavBar/AuthorizedNav.jsx
+++ src/components/NavBar/AuthorizedNav.jsx
@@ -1,26 +1,30 @@
 import { authLinks } from '@/data/authLinks';
 import Link from 'next/link';
+import { Theme } from '../Theme/Theme';
 
 export const AuthorizedNav = ({ onClose }) => {
   return (
-    <ul className="flex flex-col items-center gap-2 w-full">
-      {authLinks.map(({ name, href, icon }) => {
-        return (
-          <li
-            onClick={onClose}
-            key={name}
-            className="h-[40px] w-full bg-gradient-to-r from-[#161C2A] via-[#990078] via-50% to-[#161C2A] to-100% ..."
-          >
-            <Link
-              href={href}
-              className="flex items-center gap-6 pl-9 py-2 w-full h-[98%] bg-darkBlue"
+    <>
+      <ul className="flex flex-col items-center gap-2 w-full">
+        {authLinks.map(({ name, href, icon }) => {
+          return (
+            <li
+              onClick={onClose}
+              key={name}
+              className="h-[40px] w-full bg-gradient-to-r from-[#161C2A] via-[#990078] via-50% to-[#161C2A] to-100% ..."
             >
-              {icon}
-              <p>{name}</p>
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
+              <Link
+                href={href}
+                className="flex items-center gap-6 pl-9 py-2 w-full h-[98%] bg-darkBlue"
+              >
+                {icon}
+                <p>{name}</p>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <Theme />
+    </>
   );
 };
---
+++ src/components/Theme/Theme.jsx
@@ -0,0 +1,35 @@
+'use client';
+import { useState } from 'react';
+
+export const Theme = () => {
+  const [isChecked, setIsChecked] = useState(true);
+
+  return (
+    <ul>
+      <li className="h-[40px] w-full bg-gradient-to-r from-[#161C2A] via-[#990078] via-50% to-[#161C2A] to-100% ...">
+        <label className="flex gap-6 pl-[40px]  py-2 w-full h-[98%] bg-darkBlue">
+          <input
+            checked={isChecked}
+            type="checkbox"
+            onChange={() => {
+              setIsChecked(!isChecked);
+            }}
+          />
+          Хлопчик
+        </label>
+      </li>
+      <li className="h-[40px] w-full bg-gradient-to-r from-[#161C2A] via-[#990078] via-50% to-[#161C2A] to-100% ...">
+        <label className="flex gap-6 pl-[40px]  py-2 w-full h-[98%] bg-darkBlue">
+          <input
+            type="checkbox"
+            checked={!isChecked}
+            onChange={() => {
+              setIsChecked(!isChecked);
+            }}
+          />
+          Дівчинка
+        </label>
+      </li>
+    </ul>
+  );
+};
