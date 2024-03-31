614525b67fdb05f01052dab9a93d05588cdf2ee1
TN <tn@MacBook-Pro-TN.local>
2024-03-31 11:00:49 -0700

    create add button

---
+++ src/sections/CreateSection/CreateSection.jsx
@@ -0,0 +1,9 @@
+import { CreateAdd } from '@/components/CreateAdd/CreateAdd';
+
+export const CreateSection = () => {
+  return (
+    <section className="pt-7">
+      <CreateAdd />
+    </section>
+  );
+};
