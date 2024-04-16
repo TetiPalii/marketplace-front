43e3606e0e34d2f07913d9010f9cdb24548eb775
teitan <protas.tb@gmail.com>
2024-04-16 15:59:41 +0300

    add adaptive catalog button

--- src/sections/CreateSection/CatalogButton.tsx
+++ src/sections/CreateSection/CatalogButton.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/Button/Button";
+import CatalogIcon from "@/public/icons/CatalogIcon";
 import { ReactElement } from "react";
 
 export const CatalogButton = () => {
     return <>
-        <button>Каталог</button>
+        <Button type="filled" className="flex justify-center items-center min-w-[101px] tablet:w-[342px] h-[56.04px] relative">
+            <CatalogIcon className="tablet:absolute left-5"/>
+            <span className="hidden tablet:block">Каталог</span>
+        </Button>
     </>
 }
--- src/sections/CreateSection/CreateSection.jsx
+++ src/sections/CreateSection/CreateSection.jsx
@@ -2,14 +2,18 @@
 import { CreateAdd } from '@/components/CreateAdd/CreateAdd';
 import { useSelector } from 'react-redux';
 import { SearchField } from './SerchField';
+import { CatalogButton } from './CatalogButton';
 
 export const CreateSection = () => {
   const isLoggedIn = useSelector(state => state.user.isLoggedIn);
 
-  return <section className="pt-7">
-    <div>
-      <SearchField />
-      {isLoggedIn && <CreateAdd />}
+  return <section className="px-[15px] w-full pt-7 md:pt-9">
+    <div className='flex items-center justify-between mx-auto xl:w-[1082px]'>
+      <CatalogButton />
+      <div>
+        <SearchField />
+        {isLoggedIn && <CreateAdd />}
+      </div>
     </div>
   </section>;
 };
