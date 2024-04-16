aa6a0795198fb2e3d772a05bba5d4dccd94a917e
teitan <protas.tb@gmail.com>
2024-04-16 22:34:53 +0300

    make dropdown visibility animatiom smooth

--- src/components/Dropdown/DropdownContainer.tsx
+++ src/components/Dropdown/DropdownContainer.tsx
@@ -1,33 +1,40 @@
-import React, { useRef, ReactElement, useEffect, useState, ReactNode } from 'react';
+import React, { useRef, ReactElement, useLayoutEffect, useState, ReactNode } from 'react';
 import { CatalogDropdownItem } from './CatalogDropdownItem';
 
 const dropdownSettings = {
     headerCatalog: {
-      styles: "rounded-xl bg-[#98BAEC] flex flex-col gap-1 py-1"
+        styles: "rounded-xl bg-[#98BAEC] flex flex-col gap-1 py-1"
     }
-  };
-  
-  type DropDownType = keyof typeof dropdownSettings;
+};
+
+type DropDownType = keyof typeof dropdownSettings;
 
 export const DropdownContainer = ({
     type,
     children
-}:{
-    type?:DropDownType,
+}: {
+    type?: DropDownType,
     children?: ReactNode
 }): ReactElement => {
-    const ref = useRef<HTMLDivElement>(null);   
+    const ref = useRef<HTMLDivElement>(null);
     const [height, setHeight] = useState<number>(0);
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         if (ref.current) {
             setHeight(ref.current.clientHeight);
         }
     }, []);
 
     return (
-        <div ref={ref} className={`absolute left-0 w-full ${type? dropdownSettings[type].styles : ""}`} style={{ bottom: -height }}>
+        <div
+            ref={ref}
+            className={`
+                absolute left-0 w-full 
+                ${type ? dropdownSettings[type].styles : ""}
+            `}
+            style={{ bottom: -height }}
+        >
             {children}
         </div>
     );
-}
+};
