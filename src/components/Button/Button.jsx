8f2f3d9d932f7ea0a41bf1134f7d3c4a21a54e84
teitan <protas.tb@gmail.com>
2024-04-16 14:47:23 +0300

    add additional classnames to universal button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
-export const Button = ({ children, href }) => {
+export const Button = ({ children, href, className }) => {
   return (
     <Link
       href={href}
-      className="text-center py-3 px-7 min-w-[140px] rounded-xl  border border-[#990078] hover:bg-[#990078] hover:text-white"
+      className={`text-center py-3 px-7 min-w-[140px] rounded-xl  border border-[#990078] hover:bg-[#990078] hover:text-white ${className}`}
     >
       {children}
     </Link>
