0cac168cf3a7257928075a484945a84d7e995f06
TN <tn@macbook-pro-tn.home>
2024-05-20 12:48:28 -0700

    realized product creation functioon

--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -1,44 +1,98 @@
 "use client";
-import { useForm } from "react-hook-form";
-import { useFormState, useFormStatus } from "react-dom";
+import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Categories } from "./Categories";
 import { ProductField } from "./ProductField";
 import { ProductLable } from "./ProductLable";
 import { Condition } from "./Condition";
-import CloseIcon from "@/public/icons/CloseIcon";
-// import { createProduct } from "@/actions/createProduct";
+import { createProduct } from "@/actions/createProduct";
+import { useState } from "react";
 
+// interface ProductData {
+//   productName: string;
+//   price: string;
+//   description: string;
+//   condition: string;
+//   contactPerson: string;
+//   phoneNumber: string;
+//   email: string;
+//   location: string;
+//   category: {
+//       value: string; 
+//       label: string;
+      
+//   },
+//   file?: File
+// }
 const productSchema = z.object({
-  productName: z.string().min(3).max(50),
-  category: z.string(),
+  productName: z.string().min(5).max(50),
+  category: z.object({
+    label: z.string(),
+    value:z.string()
+  }),
+  condition:z.string(),
   price: z.string().min(0),
   description: z.string().min(10).max(500),
-  // condition: z.boolean(),
   contactPerson: z.string().min(3).max(50),
   phoneNumber: z.string(),
   email: z.string().email(),
   location: z.string().min(3).max(100),
+  file: z
+  .instanceof(File)
+  .refine((file) => file.size <= 5 * 1024 * 1024, { message: 'Размер файла должен быть не более 5MB' })
+  .refine((file) => ['image/jpeg', 'image/png', 'application/pdf'].includes(file.type), {
+    message: 'Файл должен быть изображением (JPEG/PNG) или PDF',
+  }),
+  
 });
 export type ProductSchema = z.infer<typeof productSchema>;
 
 export const ProductForm = () => {
+
   const {
     register,
     handleSubmit,
     watch,
     control,
     formState: { errors, isSubmitting },
-  } = useForm();
+  } = useForm<ProductSchema>({resolver: zodResolver(productSchema),});
 
-  const createProductAction = handleSubmit(async (data:FormData) => {
-    console.log(data); // You can send the form data to your API here
+  const onSubmit = handleSubmit(async (data) => {
+    
+    const { productName, price: productPrice, description: productDescription, condition: productType, contactPerson: sellerName, phoneNumber: sellerPhoneNumber, email: sellerEmail, location, category: { value: productCategory }, file} = data;
+    
+    const productData = { 
+      productName,
+      productPrice,
+      productDescription,
+      productCategory,
+      productType,
+      sellerEmail,
+      sellerName,
+      sellerPhoneNumber,
+      location,
+        
+    }
+    const jsonProductData = JSON.stringify(productData)
+    
+    const formData = new FormData();
+    formData.append('request', new Blob([jsonProductData], { type: 'application/json' }));
+    formData.append('files', file, file.name)
+  
+    try {
+     
+      const newProductData = await createProduct(formData)
+      console.log(newProductData)
+   } catch (error) {
+    console.log(error)
+   }
+   
   });
 
   return (
     <>
-      <form onSubmit={createProductAction} className="flex flex-col gap-2">
+      <form onSubmit={onSubmit} className="flex flex-col gap-2">
         <h1 className="text-center text-2xl font-medium">Створи оголошення</h1>
         <ProductLable inputName="Назва товару">
           <ProductField
@@ -50,6 +104,18 @@ export const ProductForm = () => {
         <ProductLable inputName="Категорія товару">
           <Categories control={control} />
         </ProductLable>
+        <Controller
+          name="file"
+          control={control}
+          render={({ field }) => (
+            <input
+              type="file"
+              onChange={(e) => {
+                if (e.target.files && e.target.files[0]) {
+                  field.onChange(e.target.files[0]);
+                }
+              }}
+            />)}/>
         <ProductLable inputName="Ціна за 1 одиницю товару">
           <ProductField type="number" register={register("price")} />
         </ProductLable>
@@ -57,7 +123,6 @@ export const ProductForm = () => {
           <textarea
             className="bg-transparent border border-formColor rounded-2xl"
             name="description"
-            id=""
             cols={30}
             rows={10}
             {...register("description")}
@@ -82,7 +147,7 @@ export const ProductForm = () => {
         </div>
 
         <button
-          type="submit"
+         
           className="text-center text-white py-3 px-7 min-w-[140px] rounded-xl bg-eggPlant hover:bg-eggPlant focus:bg-eggPlant "
         >
           Опублікувати
