f3e16cbf95acd1e59af78eec502067184bc4383f
TN <tn@macbook-pro-tn.home>
2024-06-20 22:04:34 -0700

    adde categories list styles

--- src/components/ProductForm/Categories.tsx
+++ src/components/ProductForm/Categories.tsx
@@ -3,44 +3,64 @@
 import React, { useEffect, useState } from "react";
 import { useMediaQuery } from 'react-responsive';
 import {Controller} from 'react-hook-form';
-import Select, { CSSObjectWithLabel, GroupBase, StylesConfig } from "react-select";
+import Select, {components, CSSObjectWithLabel, GroupBase, StylesConfig } from "react-select";
 import { getCategories } from "@/actions/getCategories";
 
-// const selectStyles: StylesConfig<unknown, boolean, GroupBase<unknown>> = {
-//   control: (baseStyles: CSSObjectWithLabel) => ({
-//     ...baseStyles,
-//     borderColor: "#0D0D0D",
-//     backgroundColor: "transparent",
-//     borderRadius: 16,
-//   }),
-//   menu: (baseStyles: CSSObjectWithLabel) => ({
-//     ...baseStyles,
-//     zIndex: 0,
-//   }),
-// };
 const selectStyles: StylesConfig<unknown, boolean, GroupBase<unknown>> = {
   control: (baseStyles: CSSObjectWithLabel) => ({
     ...baseStyles,
-    borderColor: "#0D0D0D",
     backgroundColor: "transparent",
     borderRadius: 20,
+    zIndex: 5,
   }),
   menu: (baseStyles: CSSObjectWithLabel) => ({
     ...baseStyles,
-    zIndex: 1,
+    zIndex: 5,
+    borderRadius: 20,
+    
+    
   }),
   option: (baseStyles: CSSObjectWithLabel, { isFocused }) => ({
     ...baseStyles,
-    border: "1px solid #990078", // Рамка для каждого элемента выпадающего списка
-    backgroundColor: isFocused ? "#E5E5E5" : "white", // Фон для активного элемента
-    ':active': {
-      ...baseStyles[':active'],
-      backgroundColor: '#B0B0B0', // Фон при клике на элемент
-    },
+    borderBottom: "1px solid #990078",
+    borderRadius: 8,
+  }),
+  placeholder: (baseStyles: CSSObjectWithLabel) => ({
+    ...baseStyles,
+    color: '#161C2A',
+    
+  }),
+  input: (baseStyles: CSSObjectWithLabel) => ({
+    ...baseStyles,
+    zIndex:0,
+  }),
+  indicatorSeparator: (baseStyles: CSSObjectWithLabel) => ({
+    ...baseStyles,
+    display: 'none', // Hide the vertical separator
   }),
 };
 
-
+const DropdownIndicator = (props) => {
+  return (
+    <components.DropdownIndicator {...props}>
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5 8L10 13L15 8"
+          stroke="#000000" // Change this to the desired color
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+     </components.DropdownIndicator>
+   )
+ }
 export const Categories = ({ control, register, id}) => {
   const [categoriesList, setCategoriesList] = useState(null);
   const isDesktopScreen = useMediaQuery({
@@ -59,30 +79,13 @@ export const Categories = ({ control, register, id}) => {
         return {
           value: categoryName,
           label: nameUkr,
+          
          
         };
       });
     }
   };
 
-  // const dynamicStyles = {
-  //   ...selectStyles,
-  //   control: (baseStyles: CSSObjectWithLabel) => ({
-  //     ...baseStyles,
-  //     borderColor: "#0D0D0D",
-  //     backgroundColor: "transparent",
-  //     borderRadius: 16,
-  //     height: isDesktopScreen ? '57px' : 'auto',
-  //     zIndex: 0,
-  //   }),
-  //   menu: (baseStyles: CSSObjectWithLabel) => ({
-  //     ...baseStyles,
-  //     zIndex: 5,
-  //     borderRadius: '20px',
-      
-    
-  //   }),
-  // };
   const dynamicStyles = {
     ...selectStyles,
     control: (baseStyles: CSSObjectWithLabel) => ({
@@ -93,17 +96,28 @@ export const Categories = ({ control, register, id}) => {
       height: isDesktopScreen ? '57px' : 'auto',
     }),
   };
+  const formatOptionLabel = ({ label }) => {
+    return <div className="text-center">
+    {label}
 
-
-
+    </div>
+  
+}
+  
   return (
     <Controller  name='category' control={control} render={({ field })=>
       <Select
-    
         {...field}
+        components={{ DropdownIndicator }}
       styles={dynamicStyles}
         options={categoriesOptions()} 
+        placeholder='Вибери категорію'
+        formatOptionLabel={formatOptionLabel}
   />}
   />
   );
 };
+
+
+
+
